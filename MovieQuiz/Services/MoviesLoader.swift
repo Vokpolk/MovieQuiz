e728861fff65eab34bc1631e95f3cb9f7e3cbcbd
Vokpolk <alexanderklopkov@gmail.com>
2025-03-29 12:44:25 +0300

    Добавлена работа с сетью. В приложении использованы фильмы из топ-250 и самых популярных фильмов IMDb

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,41 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Александр Клопков on 29.03.2025.
+//
+
+import UIKit
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let success):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: success)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let failure):
+                handler(.failure(failure))
+            }
+        }
+    }
+}
